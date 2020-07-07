4dbae714dcff3d696a8d3a878a0777ea4a5aa49e
abdullbasit <abdulljabbir.gmail.com>
2020-07-07 21:42:50 +0300

    commit

--- POTRAITANDLANDSCAPE/app/src/main/java/com/example/potraitandlandscape/MainActivity.java
+++ POTRAITANDLANDSCAPE/app/src/main/java/com/example/potraitandlandscape/MainActivity.java
@@ -1,14 +1,16 @@
-package  com.codinginflow.saveinstancestateexample;
+package  com.example.potraitandlandscape;
 
-import android.support..v7.app.AppCompactActivity;
-import  android.os.Bundle;
-import  android.view.View;
-import  android.widget.Button;
-import  android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextViewCount;
+    private TextView mTextViewContent;
     private int mCount;
 
     @Override
@@ -16,20 +18,81 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewCount = findViewById(R.id.text_view_count);
-      Button buttonDecrement = findViewById(R.id.button_decrement);
-      Button buttonIncrement = findViewById(R.id.button_increment);
+        mTextViewContent = findViewById(R.id.text_view_count);
 
-buttonDecrement.setOnClickListener(new View.OnClickListener() {
-@override
-public void onClick(View V) {
-}
-}};
-if (savedInstanceState != null) {
-  mCount = savedInstanceState.getInt("count");
-mTextViewCount.setText(string.valueOf(mCount));
-}
-}
-outstate.putIn("count", mCount);
-}
+        Button buttonDecrement = findViewById(R.id.button_decrement);
+        Button buttonIncrement = findViewById(R.id.button_increment);
+
+        buttonDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrement();
+
+            }
+        });
+
+        buttonIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increment();
+            }
+
+            private void increment() {
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt("count");
+            mTextViewContent.setText(String.valueOf(mCount));
+        }
+    }
+
+    private void decrement() {
+        mCount--;
+        mTextViewContent.setText(String.valueOf(mCount));
+    }
+
+    @Override
+    protected void  onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("count", mCount);
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume",Toast.LENGTH_SHORT).show();
+    }
 }
